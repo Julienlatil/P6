2d0e7f581c704fb744abbf8457eaf6058b37e848
Julienlatil <77979338+Julienlatil@users.noreply.github.com>
2021-07-26 15:24:37 +0200

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,23 +1,14 @@
-/////////////////AUTHENTIFICATION/////////////////////////////
-
-//création du middleware de protection des routes
-//et vérification de l'autentification du user avant l'autorisation de l'envoie des requêtes
-
-const jwt = require('jsonwebtoken'); //token
+//Protege la route et check que user est identifie. 
+//Potentiellement beaucoup de probleme, donc on utilise try/catch pour les exceptions des instructions executes
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  //try catch : il peut y avoir des erreurs
   try {
-    //extraction du token du header Authorization
-    const token = req.headers.authorization.split(' ')[1];
-    //verify : décode le token
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    //comparaison avec l'Id user
-    const userId = decodedToken.userId;
-    //si invalide alors erreur générée
-    if (req.body.userId && req.body.userId !== userId) {
+    const token = req.headers.authorization.split(' ')[1];//nous avons 2 elements Bearer et le code hash, on choisit le [1] , soit le hash qui vient apres l espace
+    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');//verifie que token valide, que "RANDOM..." correspond a celle de login()
+    const userId = decodedToken.userId;//decode l id qui est dans le token, on verifie avec une condition , s il y a un userid qu il match avec token
+    if (req.body.userId && req.body.userId !== userId) {//on verifie qu il y a userid ds requete et s il est != du token on Throw, sinon next()
       throw 'Invalid user ID';
-      //si valide alors exécution de la requête
     } else {
       next();
     }
