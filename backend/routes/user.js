dd0e5c98e4c91136c241e4de0fc97d7356148f01
Julienlatil <77979338+Julienlatil@users.noreply.github.com>
2021-07-26 15:30:22 +0200

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,19 @@
-//////////////////////////ROUTES USER////////////////////////////////
-
 const express = require('express');
-const userRouter = express.Router(); //appel méthode router de express
+const router = express.Router();//remplace app.post dans les routes par router.post
+const userCtrl = require('../controllers/user');
+const password =require('../middleware/password');
+const rateLimit = require("express-rate-limit");
+//10 possibilite de se connecter, toutes les 15 min
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, // limite de 10 tentatives
+  message:
+    "Too many accounts created from this IP, please try again after an hour"
+});
+//  apply to all requests
 
-const userCtrl = require('../controllers/user');//importation des controllers user
 
-//routes
-userRouter.post('/signup',  userCtrl.signUpUser);
-userRouter.post('/login', userCtrl.loginUser);
 
-module.exports = userRouter;
+router.post('/signup',password, userCtrl.signup);
+router.post('/login', limiter, userCtrl.login);
+module.exports = router;
