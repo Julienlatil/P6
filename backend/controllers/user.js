b874e6f2057818425d609b14834df0efbf84884c
Julienlatil <77979338+Julienlatil@users.noreply.github.com>
2021-07-26 15:22:30 +0200

    Update user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,60 +1,52 @@
-//////////////////////////CONTROLLER USER///////////////////////////
-
-const bcrypt = require('bcrypt'); //hashage du mot de passe
-const jwt = require('jsonwebtoken'); //création des token d'identification pour la session
-
-const User = require('../models/user'); //importation du model User
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const userValidator = require('./password');
 
 
-//INSCRIPTION USER
-exports.signUpUser = (req, res, next) => {
-    //cryptage du mot de passe
-    bcrypt.hash(req.body.password, 10)
-        //creation user et enregistrement dans le base de donnée
-        .then(hash => {
-            const user = new User({
-                email: req.body.email,
-                password: hash
-            });
-            user.save()
-                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-                .catch(error => res.status(400).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
+
+//fct signup qui va crypter password, va cree un nouveau user avec ce password et l email, et l'enregistre
+exports.signup = (req, res, next) => {
+  if (userValidator.isGoodPassword(req.body.password)) {
+  bcrypt.hash(req.body.password, 10) //function hashage de bcrypt, on sale 10 fois le password
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+        user.save()
+        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+  }else{
+    return res.status(404).json({ message: 'Le mot de passe doit contenir au moins un nombre, une minuscule, une majuscule et être composé de 6 caractères minimum !' });
+  }
+
 };
 
 
-//CONNEXION USER
-exports.loginUser = (req, res, next) => {
-    //chercher l'adresse mail dans la base de donnée
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            //si l'utilisateur n'est pas dans la base de donnée
-            if (!user) {
-                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-            }
-            //si l'utilisateur est trouvé alors vérification du mot de passe crypté
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    //si le mot de passe ne correspond pas
-                    if (!valid) {
-                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
-                    }
-                    //si le mot de passe corrsepond alors création d'un token d'identification
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            //création d'un token
-                            'RANDOM_TOKEN_SECRET',
-                            //valable 24h
-                            { expiresIn: '24h' }
-                        )
-                    });
-                })
-                // erreur serveur
-                .catch(error => res.status(500).json({ error }));
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })//Verifie si email est ds la bdd
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+      }
+      bcrypt.compare(req.body.password, user.password)//compare password user et bdd et renvoit id token si ok
+        .then(valid => {
+          if (!valid) {
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(//fct de jasonwebtoken: pour encoder un nouveau token
+              { userId: user._id },
+              'RANDOM_TOKEN_SECRET',//chaîne secrète de développement temporaire pour encoder notre token (à remplacer par une chaîne aléatoire beaucoup plus longue pour la production)
+              { expiresIn: '24h' }//duree de validite
+            )
+          });
         })
-        // erreur serveur
         .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 };
