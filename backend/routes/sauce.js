81d47cd7b94385ec4527b67e69818e80ab679610
Julienlatil <77979338+Julienlatil@users.noreply.github.com>
2021-07-26 15:29:51 +0200

    Update sauce.js

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,20 +1,13 @@
-//////////////////////////ROUTES SAUCE////////////////////////////////
-
 const express = require('express');
-const sauceRouter = express.Router(); //appel méthode router de express
-
-const auth = require('../middleware/auth'); //importation authentification
-const multer = require('../middleware/multer-config'); //importation multer pour la gestion des images
-
-const sauceCtrl = require('../controllers/sauce'); //importation des controllers sauce
-
-//routes
-sauceRouter.post('/',auth, multer, sauceCtrl.addSauce);
-sauceRouter.put('/:id',auth,multer, sauceCtrl.modifySauce);
-sauceRouter.get('/',auth,  sauceCtrl.getAllSauce);
-sauceRouter.get('/:id',auth,  sauceCtrl.getOneSauce);
-sauceRouter.delete('/:id', auth, sauceCtrl.deleteSauce);
-sauceRouter.post('/:id/like', auth, sauceCtrl.likeStatusSauce);
-
-
-module.exports = sauceRouter;
+const router = express.Router();//su lieu de app.post ou app.get matnt on utilisera router.post ...
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');//a rajouter a post
+const sauceCtrl = require('../controllers/sauce');
+//ordre important: auth avant multer sinon image de requete non identifie peuvent etre enregistre
+router.get('/', auth, sauceCtrl.getAllSauces);// les endpoints, on les mets dans app.js
+router.post('/', auth, multer, sauceCtrl.createSauce );
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce );
+router.delete('/:id', auth, sauceCtrl.deleteSauce );
+router.post('/:id/like', auth, sauceCtrl.likeSauce );
+module.exports = router;
